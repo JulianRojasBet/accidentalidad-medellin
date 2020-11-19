d0f1f81aa6299f1988c0f52f2a57e4604960a979
unknown <jdruize@unal.edu.co>
2020-11-19 16:20:07 -0500

    'Prediction'

--- app/server.R
+++ app/server.R
@@ -1,16 +1,34 @@
+#load(file.choose())
+
 library(shiny)
 library(dplyr)
 library(DT)
+require(mixlm)
+library(randomForest)
+library(tree)
+library(MASS)
+library(rpart)
+library(knitr)
 
+#knit("data/accidentalidad.Rmd")
+
+load("data/accidentalidad.RData")
 #map
 library(leaflet)
 library(leaflet.extras)
 
 # Server ----
 # load data
-barrios <- paste0(readLines("data/Barrio_Vereda.json",encoding="UTF-8",warn = FALSE), collapse = "")
+#barrios <- paste0(readLines("data/barrios_riesgo.json",encoding="UTF-8",warn = FALSE), collapse = "")
+altos <- paste0(readLines("data/altos.json",encoding="UTF-8",warn = FALSE), collapse = "")
+bajos <- paste0(readLines("data/bajos.json",encoding="UTF-8",warn = FALSE), collapse = "")
+medios <- paste0(readLines("data/medios.json",encoding="UTF-8",warn = FALSE), collapse = "")
+desconocidos <- paste0(readLines("data/desconocidos.json",encoding="UTF-8",warn = FALSE), collapse = "")
 data_accidentes <- read.csv(file = "data/incidentes_medellin.csv", fileEncoding = "utf-8");
 resumen_accidentes <- read.csv(file = "data/Resumen-accidentalidad.csv", sep = ";")
+meses <- read.csv(file = "data/meses.csv", sep = ";")
+fechas_importantes <- read.csv(file = "data/fechas_importantes_2020.csv", sep=";")
+
 
 # Add date complete to data
 resumen_accidentes$FECHA <- (ISOdate(resumen_accidentes$PERIODO, resumen_accidentes$MES, 
@@ -36,16 +54,16 @@ function(input, output, session) {
     dateFinish <- format(as.Date(input$dates[2] + 1, origin = "1970-01-01"), "%Y-%m-%d")
     data <- resumen_accidentes %>% 
       filter(FECHA >= dateStart & FECHA <= dateFinish) %>%
-      select("PERIODO", "MES", "DIA", "COMUNA", "Riesgo", "Es.fecha.importante",
-             "Numero.de.accidentes", "Accidentes.con.Heridos", "Accidentes.con.solo.daños",
-             "Accidentes.con.muertos")
+      dplyr::select("PERIODO", "MES", "DIA", "COMUNA", "BARRIO", "Riesgo", "Es.fecha.importante",
+             "Suma.de.Numero.de.accidentes", "Suma.de.Numero.de.Heridos", "Suma.de.Número.de.solo.daños",
+             "Suma.de.Número.de.muertos")
     
     sketch <- htmltools::withTags(
       table(
-        tableHeader(c("Año", "Mes", "Día", "Comuna", "Riesgo", "Fecha importante", 
+        tableHeader(c("Año", "Mes", "Día", "Comuna", "Barrio", "Riesgo", "Fecha importante", 
                       "Número accidentes",  "Accidentes con heridos", "Accidentes con solo daños", 
                       "Accidentes con muertos")),
-        tableFooter(c("Subtotal","","","","","",0,0,0,0))
+        tableFooter(c("Subtotal","","","","","","",0,0,0,0))
       ))
     
     DT::datatable(
@@ -55,6 +73,7 @@ function(input, output, session) {
       filter = "top", options = list(
         deferRender = TRUE,
         searching = TRUE,
+        scrollX = TRUE,
         filter = list(position = "top", clear = FALSE),
         pageLength = 10,
         sDom  = '<"top">lrt<"bottom">ip',
@@ -65,11 +84,6 @@ function(input, output, session) {
         footerCallback = JS(
           "function( tfoot, data, start, end, display ) {",
           "var api = this.api(), data;",
-          "$( api.column(6).footer()).html(",
-          "api.column(6).data().reduce( function ( a, b ) {",
-          "return a + b;",
-          "} )",
-          ");",
           "$( api.column(7).footer()).html(",
           "api.column(7).data().reduce( function ( a, b ) {",
           "return a + b;",
@@ -85,6 +99,11 @@ function(input, output, session) {
           "return a + b;",
           "} )",
           ");",
+          "$( api.column(10).footer()).html(",
+          "api.column(10).data().reduce( function ( a, b ) {",
+          "return a + b;",
+          "} )",
+          ");",
           "}")
       ),
       rownames = FALSE
@@ -97,48 +116,292 @@ function(input, output, session) {
   })
   
   # Predict
+  #Modelo1
+  modelo1 <- rpart(`Numero de accidentes`~MES+DIA+DIA_NOMBRE+COMUNA+puente_festivo+puente_reyes+semana_santa+feria_flores+puente_semana_santa+festivo_entre_semana+no_festivo_importante+Riesgo, data=accidentalidad, method = 'anova')
+ 
+  #Modelo2
+  modelo2<-lm(`Accidente con solo daños`~MES+DIA+DIA_NOMBRE+COMUNA+puente_festivo+puente_reyes+semana_santa+feria_flores+puente_semana_santa+festivo_entre_semana+no_festivo_importante+Riesgo+`Numero de accidentes`, data=accidentalidad)
+ 
+  #Creando variable accidentes_heridos_o_muertos
+  accidentalidad<-accidentalidad %>%
+   dplyr::select(puente_festivo,puente_reyes,semana_santa,feria_flores,puente_semana_santa,festivo_entre_semana,no_festivo_importante,`Accidente de muertos`,MES,DIA,DIA_NOMBRE,COMUNA,Riesgo,`Numero de accidentes`,PERIODO,`Accidentes con Heridos`) %>%
+   mutate(accidentalidad,accidentes_heridos_o_muertos=`Accidente de muertos`+`Accidentes con Heridos`)
+ 
+  #Modelo 3
+  modelo3<-lm(accidentes_heridos_o_muertos~MES+DIA+DIA_NOMBRE+COMUNA+puente_festivo+puente_reyes+semana_santa+feria_flores+puente_semana_santa+festivo_entre_semana+no_festivo_importante+Riesgo+`Numero de accidentes`, data=accidentalidad)
   
+  #function(input, output, session) {
+  
+  output$secondSelection <- renderUI({
+    selectInput("SelectedBarrio", h4("Seleccione el barrio"), choices = unique(resumen_accidentes %>% 
+                                                                                  filter(COMUNA == input$SelectedComuna) %>%
+                                                                                  dplyr::select("BARRIO")))
+  })
+  
+  output$value_prediction <- eventReactive(input$prediction, {
+    COMUNA <- input$SelectedComuna
+    
+    Riesgo <- unique(resumen_accidentes %>% 
+                       filter(COMUNA == input$SelectedComuna & BARRIO == input$SelectedBarrio) %>%
+                       dplyr::select("Riesgo"))
+    
+    
+    if(input$SelectedOption=="Mes"){
+      DATE <- paste("Mes:", input$SelectedMonth, sep=" ")
+      get_month <- meses  %>% 
+          filter(nombre_mes==input$SelectedMonth) %>%
+          dplyr::select("fecha_inicial", "fecha_final")
+      startDate <- as.POSIXct(format(as.Date(get_month[[1]][1], origin = "1970-01-01"), "%Y-%m-%d"))
+      endDate <- as.POSIXct(format(as.Date(get_month[[2]][1], origin = "1970-01-01"), "%Y-%m-%d"))
+      all_dates = seq(startDate, endDate, by="day")
+
+      for (j in 1:length(all_dates)) {
+        fechas <- fechas_importantes %>% 
+          filter(FECHA==all_dates[j]) %>%
+          dplyr::select("puente_festivo", "puente_reyes", "semana_santa", "puente_semana_santa", "feria_flores", "festivo_entre_semana", "no_festivo_importante")
+        
+        puente_festivo <- toString(fechas[[1]][1])
+        puente_reyes <- toString(fechas[[2]][1])
+        semana_santa <- toString(fechas[[3]][1])
+        puente_semana_santa <- toString(fechas[[4]][1])
+        feria_flores <- toString(fechas[[5]][1])
+        festivo_entre_semana <- toString(fechas[[6]][1])
+        no_festivo_importante <- toString(fechas[[7]][1])
+        date_split <- strsplit(toString(all_dates[j]), '-')[[1]]
+        
+        MES <-date_split[2]
+        if (strsplit(MES, '0')[[1]][1]=="") {
+          MES <- strsplit(MES, '0')[[1]][2]
+        }
+        DIA <-date_split[3]
+        if (strsplit(DIA, '0')[[1]][1]=="") {
+          DIA <- strsplit(DIA, '0')[[1]][2]
+        }
+        MES <- as.integer(MES)
+        DIA <- as.integer(DIA)
+        DIA_NOMBRE <- toupper(weekdays(as.Date(all_dates[j])))
+        if (j==1) {
+          df_model1 <- data.frame(MES, DIA, DIA_NOMBRE, COMUNA,puente_festivo, puente_reyes,semana_santa, feria_flores, puente_semana_santa, festivo_entre_semana, no_festivo_importante,Riesgo)
+        } else {
+          add_row <- data.frame(MES, DIA, DIA_NOMBRE, COMUNA,puente_festivo, puente_reyes,semana_santa, feria_flores, puente_semana_santa, festivo_entre_semana, no_festivo_importante,Riesgo)
+          df_model1 <- rbind(df_model1, add_row)
+        }
+      }
+    }
+    
+    
+    if(input$SelectedOption=="Semana"){
+      DATE <-  paste(toString(input$datesWeek[1]), toString(input$datesWeek[2]), sep="/")
+      startDate <- as.POSIXct(format(as.Date(input$datesWeek[1], origin = "1970-01-01"), "%Y-%m-%d"))
+      endDate <- as.POSIXct(format(as.Date(input$datesWeek[2], origin = "1970-01-01"), "%Y-%m-%d"))
+      all_dates = seq(startDate, endDate, by="day")
+      
+      for (j in 1:length(all_dates)) {
+        fechas <- fechas_importantes %>% 
+          filter(FECHA==all_dates[j]) %>%
+          dplyr::select("puente_festivo", "puente_reyes", "semana_santa", "puente_semana_santa", "feria_flores", "festivo_entre_semana", "no_festivo_importante")
+        puente_festivo <- toString(fechas[[1]][1])
+        puente_reyes <- toString(fechas[[2]][1])
+        semana_santa <- toString(fechas[[3]][1])
+        puente_semana_santa <- toString(fechas[[4]][1])
+        feria_flores <- toString(fechas[[5]][1])
+        festivo_entre_semana <- toString(fechas[[6]][1])
+        no_festivo_importante <- toString(fechas[[7]][1])
+        date_split <- strsplit(toString(all_dates[j]), '-')[[1]]
+        
+        MES <-date_split[2]
+        if (strsplit(MES, '0')[[1]][1]=="") {
+          MES <- strsplit(MES, '0')[[1]][2]
+        }
+        DIA <-date_split[3]
+        if (strsplit(DIA, '0')[[1]][1]=="") {
+          DIA <- strsplit(DIA, '0')[[1]][2]
+        }
+        MES <- as.integer(MES)
+        DIA <- as.integer(DIA)
+        DIA_NOMBRE <- toupper(weekdays(as.Date(all_dates[j])))
+        
+        if (j==1) {
+          df_model1 <- data.frame(MES, DIA, DIA_NOMBRE, COMUNA,puente_festivo, puente_reyes,semana_santa, feria_flores, puente_semana_santa, festivo_entre_semana, no_festivo_importante,Riesgo)
+        } else {
+          add_row <- data.frame(MES, DIA, DIA_NOMBRE, COMUNA,puente_festivo, puente_reyes,semana_santa, feria_flores, puente_semana_santa, festivo_entre_semana, no_festivo_importante,Riesgo)
+          df_model1 <- rbind(df_model1, add_row)
+        }
+      }
+    }
+    
+    
+    if(input$SelectedOption=="Dia"){
+      DATE <- toString(input$dateDay)
+      date_split <- strsplit(toString(input$dateDay), '-')[[1]]
+      MES <- toString(date_split[2])
+      if (strsplit(MES, '0')[[1]][1]=="") {
+        MES <- strsplit(MES, '0')[[1]][2]
+      }
+      DIA <- toString(date_split[3])
+      if (strsplit(DIA, '0')[[1]][1]=="") {
+        DIA <- strsplit(DIA, '0')[[1]][2]
+      }
+      MES <- as.integer(MES)
+      DIA <- as.integer(DIA)
+      DIA_NOMBRE<-  toupper(weekdays(as.Date(input$dateDay)))
+      fecha <- fechas_importantes %>% 
+        filter(FECHA==input$dateDay) %>%
+        dplyr::select("puente_festivo", "puente_reyes", "semana_santa", "puente_semana_santa", "feria_flores", "festivo_entre_semana", "no_festivo_importante")
+      puente_festivo <- toString(fecha[[1]][1])
+      puente_reyes <- toString(fecha[[2]][1])
+      semana_santa <- toString(fecha[[3]][1])
+      puente_semana_santa <- toString(fecha[[4]][1])
+      feria_flores <- toString(fecha[[5]][1])
+      festivo_entre_semana <- toString(fecha[[6]][1])
+      no_festivo_importante <- toString(fecha[[7]][1])
+      
+      df_model1 <- data.frame(MES, DIA, DIA_NOMBRE, COMUNA,puente_festivo, puente_reyes,semana_santa, feria_flores, puente_semana_santa, festivo_entre_semana, no_festivo_importante,Riesgo)
+      }
+    
+    
+    model1<-predict(modelo1, df_model1)
+    df_model2 <- cbind(df_model1,`Numero de accidentes`=model1)
+    model2<-predict(modelo2, df_model2)
+    model3<-predict(modelo3, df_model2)
+    m1(DATE)
+    m2(round(sum(model1),0))
+    m3(round(sum(model2),0))
+    m4(round(sum(model3),0))
+    
+    textoSalida <- paste(DATE, "Numero de accidentes:", sum(model1), "Accidente con solo daños: ", sum(model2), "Accidentes con heridos o muertos:", sum(model3), sep=" ")
+    
+    })
+  
+  m1 <- reactiveVal(0)
+  m2 <- reactiveVal(0)
+  m3 <- reactiveVal(0)
+  m4 <- reactiveVal(0)
+  
+  output$outDate <- renderValueBox({
+    valueBox(
+      m1(), "Fecha", icon = icon("calendar-day"),
+      color = "blue"
+    )
+  })
+  output$outModel1 <- renderValueBox({
+    valueBox(
+      m2(), "Numero de accidentes", icon = icon("sort-numeric-up"),
+      color = "yellow"
+    )
+  })
+  output$outModel2 <- renderValueBox({
+    valueBox(
+      m3(), "Accidente con solo daños", icon = icon("exclamation-triangle"),
+      color = "orange"
+    )
+  })
+  output$outModel3 <- renderValueBox({
+    valueBox(
+      m4(), "Accidentes con heridos o muertos", icon = icon("dizzy"),
+      color = "red"
+    )
+  })
+  
+
   # Map
   
   output$map_group <- renderLeaflet({
     pal <- colorNumeric("viridis", NULL)
-    
-    
-    
-    
     leaflet() %>%
       setView(-75.595178, 6.26, 12) %>%
       addTiles() %>%
-      addGeoJSONChoropleth(barrios,
-                           #valueProperty = "SHAPEAREA",
-                           valueProperty = JS(
-                             'function(feature){
-                                         var props = feature.properties;
-                                         return Math.log(props.SHAPEAREA);
-                                      }'
-                           ),
-                           labelProperty = "NOMBRE",
-                           scale = c("YELLOW", "red"),
-                           popupProperty = propstoHTMLTable(
-                             props = c("NOMBRE", "CODIGO", "NOMBRE_COM"),
-                             table.attrs = list(class = "table table-striped table-bordered"),
-                             drop.na = TRUE
-                           ),
-                           #stroke
-                           color = "#ffffff", weight = 1, fillOpacity = 0.3,
-                           highlightOptions = highlightOptions(
-                             weight = 2, color = "#000000",
-                             fillOpacity = 0.5, opacity = 1,
-                             bringToFront = TRUE, sendToBack = TRUE),
-                           pathOptions = pathOptions(
-                             showMeasurements = TRUE),
-                           legendOptions = legendOptions(
-                             title = "log(area (m2))",
-                             numberFormatOptions = list(
-                               style = "decimal",
-                               maximumFractionDigits = 0
-                             )
-                           ),
+      addGeoJSONv2(
+        altos,
+        labelProperty = "NOMBRE",
+        popupProperty = propstoHTMLTable(
+          props = c("NOMBRE", "NOMBRE_COM", "RIESGO"),
+          table.attrs = list(class = "table table-striped table-bordered"),
+          drop.na = TRUE
+        ),
+        color = "#ffffff",
+        weight = 1,
+        fillOpacity = 0.3,
+        fillColor = "red",
+        highlightOptions = highlightOptions(
+          weight = 2,
+          color = "#000000",
+          fillOpacity = 0.5,
+          opacity = 1,
+          bringToFront = TRUE,
+          sendToBack = TRUE
+        ),
+        pathOptions = pathOptions(showMeasurements = TRUE),
+        
+      ) %>%
+      addGeoJSONv2(
+        medios,
+        labelProperty = "NOMBRE",
+        popupProperty = propstoHTMLTable(
+          props = c("NOMBRE", "NOMBRE_COM", "RIESGO"),
+          table.attrs = list(class = "table table-striped table-bordered"),
+          drop.na = TRUE
+        ),
+        color = "#ffffff",
+        weight = 1,
+        fillOpacity = 0.5,
+        fillColor = "orange",
+        highlightOptions = highlightOptions(
+          weight = 2,
+          color = "#000000",
+          fillOpacity = 0.5,
+          opacity = 1,
+          bringToFront = TRUE,
+          sendToBack = TRUE
+        ),
+        pathOptions = pathOptions(showMeasurements = TRUE),
+        
+      ) %>%
+      addGeoJSONv2(
+        bajos,
+        labelProperty = "NOMBRE",
+        popupProperty = propstoHTMLTable(
+          props = c("NOMBRE", "NOMBRE_COM", "RIESGO"),
+          table.attrs = list(class = "table table-striped table-bordered"),
+          drop.na = TRUE
+        ),
+        color = "#ffffff",
+        weight = 1,
+        fillOpacity = 0.2,
+        fillColor = "yellow",
+        highlightOptions = highlightOptions(
+          weight = 2,
+          color = "#000000",
+          fillOpacity = 0.5,
+          opacity = 1,
+          bringToFront = TRUE,
+          sendToBack = TRUE
+        ),
+        pathOptions = pathOptions(showMeasurements = TRUE),
+        
+      ) %>%
+      addGeoJSONv2(
+        desconocidos,
+        labelProperty = "NOMBRE",
+        popupProperty = propstoHTMLTable(
+          props = c("NOMBRE", "NOMBRE_COM", "RIESGO"),
+          table.attrs = list(class = "table table-striped table-bordered"),
+          drop.na = TRUE
+        ),
+        color = "#ffffff",
+        weight = 1,
+        fillOpacity = 0.3,
+        fillColor = "gray",
+        highlightOptions = highlightOptions(
+          weight = 2,
+          color = "#000000",
+          fillOpacity = 0.5,
+          opacity = 1,
+          bringToFront = TRUE,
+          sendToBack = TRUE
+        ),
+        pathOptions = pathOptions(showMeasurements = TRUE),
+        
       )
   })
   output$click_info<- renderText({
--- app/ui.R
+++ app/ui.R
@@ -5,6 +5,8 @@ library(rintrojs)
 library(leaflet)
 
 
+barrios_riesgo <- read.csv(file = "data/barrios_riesgo.csv",fileEncoding = "utf-8", sep = ",")
+
 # dropdown ----
 dropdownMenu <- 
   dropdownMenu(
@@ -81,14 +83,91 @@ sidebar <- dashboardSidebar(
       )
     )
   
+# box prediction ---
+  prediction_box <-
+    box(title="Predicción",
+      status = "success", solidHeader = TRUE, width = 12,
+      fluidRow(
+        column(6, offset = 0,
+               div(style="text-align: center; width: 50%; display: block; 
+            margin-left: auto; margin-right: auto; margin-top: 30px;",
+            selectInput("SelectedComuna",
+               label = h4("Seleccione la comuna"),
+               choices = c(unique(barrios_riesgo[["COMUNA"]])),
+               selected = "Comuna"
+            )
+          )
+        ),
+        column(6, offset = 0,
+               div(style="text-align: center; width: 50%; display: block; 
+            margin-left: auto; margin-right: auto; margin-top: 30px;",
+                   uiOutput("secondSelection")
+               )
+        ),
+        column(6, offset = 0,
+               div(style="text-align: center; width: 50%; display: block; 
+            margin-left: auto; margin-right: auto; margin-top: 30px;",
+                   selectInput("SelectedOption",
+                               label = h4("Seleccione el tipo de visualización"),
+                               choices = c("Mes" = "Mes", "Semana" = "Semana", "Día" = "Dia"),
+                               selected = "Mes"
+                   )
+               )
+        ),
+          column(6, offset = 0,
+                 div(style="text-align: center; width: 50%; display: block; 
+              margin-left: auto; margin-right: auto; margin-top: 30px;",
+             conditionalPanel(condition="input.SelectedOption=='Mes'",
+                              selectInput("SelectedMonth",
+                                          label = h4("Seleccione el tipo de visualización"),
+                                          choices = list("Enero","Febrero","Marzo","Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"),
+                                          selected = "Mes"
+                              )
+             ),     
+             conditionalPanel(condition="input.SelectedOption=='Semana'",
+                    dateRangeInput(
+                      inputId="datesWeek",
+                      label = h4("Seleccione el primer y ultimo día de la semana que desea visualizar"),
+                      start = "2020-01-01", end = "2020-12-31",
+                      min = "2020-01-01", max = "2020-12-31",
+                      language = "es", format = "dd/MM/yyyy",
+                      separator = " - "
+             )
+             ),
+             conditionalPanel(condition="input.SelectedOption=='Dia'",
+                              dateInput(inputId="dateDay", label = h4("Seleccione el día que desea visualizar"), value = "2020-01-01"),
+             ),  
+           )
+        ),
+        column(12, offset = 0,
+               div(style="text-align: center; width: 50%; display: block; 
+            margin-left: auto; margin-right: auto; margin-top: 30px;",
+                   actionButton('prediction', 'Predecir', icon = icon("filter"),
+                                style = "color: white", class = "btn btn-success"),
+               )
+        ),
+
+      ),
+      fluidRow(
+        #column(12, offset = 0,
+        #      div(style="text-align: center; width: 95%; display: block; 
+        #    margin-left: auto; margin-right: auto; margin-top: 60px;",
+        div(style="visibility:hidden", verbatimTextOutput("value_prediction")),
+        column(12,  style="margin-top: 10px; display: block; margin-left: 115px;",valueBoxOutput("outDate", width=4),
+               valueBoxOutput("outModel1", width=2),
+               valueBoxOutput("outModel2", width=2),
+               valueBoxOutput("outModel3", width=2)),
+      )
+    )
+  
 #box map group
-  map_goup_box <- box(title="Mapa de los barrios de Medellin",
+  map_goup_box <- box(title="Mapa con agrupamiento de los barrios de Medellin",
                       status = "success", solidHeader = TRUE, width = 12,
                         fluidRow(
                           column(12,offset = 0,
                                  div(style="width: 95%;margin-left: auto; margin-right: auto;",
-                          leafletOutput("map_group"),
-                          verbatimTextOutput("click_info"),
+                          leafletOutput("map_group",height = "75vh"),
+                          #verbatimTextOutput("click_info"),
                                  )
                           )
                         )
@@ -99,7 +178,7 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "visualization", fluidRow(visualization_box)),
-    tabItem(tabName = "prediction", fluidRow()),
+    tabItem(tabName = "prediction", fluidRow(prediction_box)),
     tabItem(tabName = "grouping", fluidRow(map_goup_box))
   )
 )
